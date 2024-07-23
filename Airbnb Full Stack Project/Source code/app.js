7d452caec4620f29831f5b3e09178dcdee26d1cd
Snehal Gaikwad <161202223+SnehalGaikwad-04@users.noreply.github.com>
2024-07-23 22:16:27 +0530

    Update app.js

--- Airbnb Full Stack Project/Source code/app.js
+++ Airbnb Full Stack Project/Source code/app.js
@@ -7,6 +7,8 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
+const {listingSchema, reviewSchema} = require('./schema.js');
+const Review = require("./models/review.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -32,6 +34,26 @@ app.get("/", (req,res) => {
     res.send("hi i am root");
 });
 
+const validateListing = (req, res, next) => {
+    let {error} = listingSchema.validate(req.body);
+        if (error){
+            let errMsg = error.details.map((el) => el.message).join(",");
+            throw new ExpressError(400, errMsg);
+        }else{
+            next();
+        }
+}
+
+const validateReview = (req, res, next) => {
+    let {error} = reviewSchema.validate(req.body);
+        if (error){
+            let errMsg = error.details.map((el) => el.message).join(",");
+            throw new ExpressError(400, errMsg);
+        }else{
+            next();
+        }
+}
+
 //Index Route
 app.get("/listings", wrapAsync(async (req,res) => {
     const allListings = await Listing.find({});
@@ -46,19 +68,16 @@ app.get("/listings/new", (req,res) => {
 //Show route
 app.get("/listings/:id", wrapAsync(async (req,res) => {
     let {id} = req.params;
-    const listing = await Listing.findById(id);
+    const listing = await Listing.findById(id).populate("reviews");
     res.render("listings/show.ejs", {listing} );
 }));
 
 //Create route
 app.post(
     "/listings",
+    validateListing,
     wrapAsync(async(req,res,next) => {
-        if(!req.body.listing){
-            throw new ExpressError(400, "Send valid data for listing.")
-        }
         const newListing= new Listing(req.body.listing);
-        
         await newListing.save();
         res.redirect("/listings");        
 })
@@ -72,10 +91,9 @@ app.get("/listings/:id/edit", wrapAsync(async (req,res) => {
 }));
 
 //Update Route
-app.put("/listings/:id", wrapAsync(async(req,res) => {
-    if(!req.body.listing){
-        throw new ExpressError(400, "Send valid data for listing.")
-    }
+app.put("/listings/:id", 
+    validateListing,
+    wrapAsync(async(req,res) => {
     let {id} = req.params;
     await Listing.findByIdAndUpdate(id, {...req.body.listing});
     res.redirect(`/listings/${id}`);
@@ -87,6 +105,20 @@ app.delete("/listings/:id", wrapAsync(async(req,res) => {
     let deletedListing = await Listing.findByIdAndDelete(id);
     console.log(deletedListing );
     res.redirect("/listings");
+}));
+
+//Reviews
+//Post Routex
+app.post("/listings/:id/reviews", validateReview, wrapAsync(async(req,res) => {
+    let listing = await Listing.findById(req.params.id);
+    let newReview = new Review(req.body.review);
+
+    listing.reviews.push(newReview);
+
+    await newReview.save();
+    await listing.save();
+
+    res.redirect(`/listings/${listing._id}`);
 }));
 
 // app.get("/testListing", async(req,res) => {
